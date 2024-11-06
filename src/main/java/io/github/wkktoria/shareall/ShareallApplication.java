09a9b2ddccf3f553469570fa189cdab0891a8204
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-06 15:17:56 +0100

    Init project

---
+++ src/main/java/io/github/wkktoria/shareall/ShareallApplication.java
@@ -0,0 +1,13 @@
+package io.github.wkktoria.shareall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareallApplication.class, args);
+	}
+
+}
