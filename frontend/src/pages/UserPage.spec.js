e17975272d120624391b47172c44aff6275612d3
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-12 12:30:06 +0100

    Load user on user page on frontend

--- frontend/src/pages/UserPage.spec.js
+++ frontend/src/pages/UserPage.spec.js
@@ -1,6 +1,22 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import UserPage from "./UserPage";
+import * as apiCalls from "../api/apiCalls";
+
+const mockSuccessGetUser = {
+  data: {
+    id: 1,
+    username: "user1",
+    displayName: "display1",
+    image: "profile1.png",
+  },
+};
+
+const match = {
+  params: {
+    username: "user1",
+  },
+};
 
 describe("UserPage", () => {
   describe("Layout", () => {
@@ -8,6 +24,27 @@ describe("UserPage", () => {
       const { queryByTestId } = render(<UserPage />);
       const UserPageDiv = queryByTestId("userpage");
       expect(UserPageDiv).toBeInTheDocument();
+    });
+
+    it("displays the displayName@username when user data loaded", async () => {
+      apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
+      const { findByText } = render(<UserPage match={match} />);
+      const text = await findByText("display1@user1");
+      expect(text).toBeInTheDocument();
+    });
+  });
+
+  describe("Lifecycle", () => {
+    it("calls getUser when it is rendered", () => {
+      apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
+      render(<UserPage match={match} />);
+      expect(apiCalls.getUser).toHaveBeenCalledTimes(1);
+    });
+
+    it("calls getUser for user1 when it is rendered with user1 in match", () => {
+      apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
+      render(<UserPage match={match} />);
+      expect(apiCalls.getUser).toHaveBeenCalledWith("user1");
     });
   });
 });
