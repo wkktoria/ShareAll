0f1ba6c3f5709accd8c4588c01fdd8ba382c4bb9
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-11-13 22:32:14 +0100

    Add file input on user profile on frontend

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -27,6 +27,7 @@ const ProfileCard = (props) => {
               label={`Change display name for ${username}`}
               onChange={props.onChangeDisplayName}
             />
+            <input className="form-control mt-2" type="file" />
           </div>
         )}
         {showEditButton && (
